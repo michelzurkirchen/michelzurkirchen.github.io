f94e6328ab1065dec3b582b5761d334265d84ed1
michelzurkirchen <michel.zurkirchen@gmail.com>
2020-09-27 11:01:27 +0200

    Moving sourcehub to GitHub Pages repo

---
+++ app/routes.py
@@ -0,0 +1,10 @@
+from flask import render_template, session
+from app import app
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/facebook")
+def facebook_home():
+    return render_template("facebook.html")
